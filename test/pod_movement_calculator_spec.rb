19d08ad4bc2166503112c3b7269afc74c116b97e
Regis Seki <regisseki@gmail.com>
2020-02-03 13:02:49 -0300

    WIP pod calculator test

--- test/pod_movement_calculator_spec.rb
+++ test/pod_movement_calculator_spec.rb
@@ -3,10 +3,19 @@ require 'pry'
 
 RSpec.describe 'PodMovementCalculator' do
   context '#execute' do
-    describe '' do
-      it '' do
-        result = PodMovementCalculator.new.execute(["1", "2", "N"], ["L", "M", "L", "M", "L", "M", "L", "M", "M"])
-        binding.pry
+    before do
+    end
+
+    describe 'using specification document first input entrance' do
+      it 'should return an array with end_position as 1 3 N' do
+        result = PodMovementCalculator.new.execute(['1', '2', 'N'], ['L', 'M', 'L', 'M', 'L', 'M', 'L', 'M', 'M'])
+        expect(result).to eq ['1', '3', 'N']
+      end
+    end
+
+    describe 'using second input entrance' do
+      it 'should return an array with 5 1 E' do
+
       end
     end
   end
