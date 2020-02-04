c5c4c86d72ca4c373ebeeec7d4f1be74f9ddc8b2
Regis Seki <regisseki@gmail.com>
2020-02-03 23:48:09 -0300

    Exploration end message

--- bin/start_exploration.rb
+++ bin/start_exploration.rb
@@ -13,25 +13,25 @@ output_message.send_message(' Are you ready? (y):')
 
 answear = gets.chomp
 if answear === 'y'
-  output_message.send_message(' Perfect! How can i call you:')
+  output_message.send_message('Perfect! How can i call you:')
   nick_name = gets.chomp
-  output_message.send_message(" Hello #{nick_name.capitalize}, nice to have you here.")
-  output_message.send_message(' Our mission is to explore Mars area, to do this you need to give me the max lenght of the area that we gonna explore now!')
-  output_message.send_message(' Give me the max size with 2 numbers separeted with space. For example: 5 5:')
+  output_message.send_message("Hello #{nick_name.capitalize}, nice to have you here.")
+  output_message.send_message('Our mission is to explore Mars area, to do this you need to give me the max lenght of the area that we gonna explore now!')
+  output_message.send_message('Give me the max size with 2 numbers separeted with space. For example: 5 5:')
   max_exploration_area = input_valid.valid_max_exploration_area(gets.chomp)
-  output_message.send_message(" Good! Now let's deploy the first pod. \n Tell me the first pod initial location, please. \n Should be 3 characters, first the x position, second the y position and the cardinal coordenate as N from North, S from South, O for Oest and W for West. For example: 1 2 N :")
+  output_message.send_message("Good! Now let's deploy the first pod. \n Tell me the first pod initial location, please. \n Should be 3 characters, first the x position, second the y position and the cardinal coordenate as N from North, S from South, O for Oest and W for West. For example: 1 2 N :")
   first_pod_initial_position = input_valid.valid_initial_position(gets.chomp)
-  output_message.send_message(" Now give me the movements you want the first pod to do. \n You can use L (left) or R (right) to turn 90 degrees for respective side and M for move 1 space. For example LMLMLMLMM, it means the pod will turn left and move one space and than turn right and move one more space :")
+  output_message.send_message("Now give me the movements you want the first pod to do. \n You can use L (left) or R (right) to turn 90 degrees for respective side and M for move 1 space. For example LMLMLMLMM, it means the pod will turn left and move one space and than turn right and move one more space :")
   first_pod_movements = input_valid.valid_movements(gets.chomp)
-  output_message.send_message(' We gonna sent the second pod, give the initial position. Ex: 3 3 E:')
+  output_message.send_message('We gonna sent the second pod, give the initial position. Ex: 3 3 E:')
   second_pod_initial_position = input_valid.valid_initial_position(gets.chomp)
-  output_message.send_message(' And now the commands. Example: MMRMMRMRRM:')
+  output_message.send_message('And now the commands. Example: MMRMMRMRRM:')
   second_pod_movements = input_valid.valid_movements(gets.chomp)
-  output_message.send_message('Pods are all finished it moves and actual position is:')
   result = []
-  result.push(pod_movement_calculator.execute(first_pod_initial_position, first_pod_movements))
-  result.push(pod_movement_calculator.execute(second_pod_initial_position, second_pod_movements))
-  print result
+  first_pod_end_position = pod_movement_calculator.execute(first_pod_initial_position, first_pod_movements)
+  second_pod_end_position = pod_movement_calculator.execute(second_pod_initial_position, second_pod_movements)
+  output_message.send_message('Pods are all finished it moves and actual position is:')
+  output_message.send_message("first pod #{first_pod_end_position[0]} #{first_pod_end_position[1]} #{first_pod_end_position[2]} and second pod #{second_pod_end_position[0]} #{second_pod_end_position[1]} #{second_pod_end_position[2]}")
 else
-  output_message.send_message(" Sorry, i think you are very busy doing something better! \n If you change your mind, see you later!")
+  output_message.send_message("Sorry, i think you are very busy doing something better! \n If you change your mind, see you later!")
 end
