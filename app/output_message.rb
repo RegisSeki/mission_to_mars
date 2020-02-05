88d5f261fc581bf644640fb62a99516d23aeb8ac
Regis Seki <regisseki@gmail.com>
2020-02-05 15:46:34 -0300

    Added more specified message method

--- app/output_message.rb
+++ app/output_message.rb
@@ -1,10 +1,15 @@
 class OutputMessage
   def send_message(message)
-    puts "#{message} \n"
+    puts " #{message} \n"
   end
 
   def send_message_exit(message)
-    puts "#{message} \n"
+    puts " #{message} \n"
     exit
+  end
+
+  def send_message_clear(message)
+    system("clear")
+    puts " #{message} \n"
   end
 end
